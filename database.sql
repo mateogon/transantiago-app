5841ecfc9f6802fba70cfec750004c99501df9d4
AkumuKernel <marcelo.fuenzalida@proton.me>
2024-10-21 08:11:50 -0300

    Avance BD ARRR

--- database.sql
+++ database.sql
@@ -3,12 +3,109 @@ CREATE EXTENSION IF NOT EXISTS postgis;
 
 #Extensión para dijkstra
 CREATE EXTENSION IF NOT EXISTS pgrouting;
+
+#Extensión para UUID
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+--------------------------------------------------------------
+#Paraderos
+CREATE TABLE paraderos (
+    paradero VARCHAR(10) PRIMARY KEY,
+    geom GEOMETRY(Point, 4326)
+);
 --------------------------------------------------------------
 #Recorridos
 CREATE TABLE recorridos (
-    paradero SERIAL PRIMARY KEY,
-    descripcion VARCHAR(50),
-    geom GEOMETRY(Geometry, 4326)
+    id SERIAL PRIMARY KEY,
+    origen VARCHAR(10) REFERENCES paraderos(paradero),
+    destino VARCHAR(10) REFERENCES paraderos(paradero),
+    distancia DOUBLE PRECISION,  -- Peso de la arista, puede ser la distancia entre los paraderos
+    geom GEOMETRY(LineString, 4326)
+);
+--------------------------------------------------------------
+#Aglomeracion
+CREATE TABLE aglomeracion (
+    paradero VARCHAR(255) PRIMARY KEY,
+    Comuna VARCHAR(255),
+    "5:30:00" FLOAT,
+    "6:00:00" FLOAT,
+    "6:30:00" FLOAT,
+    "7:00:00" FLOAT,
+    "7:30:00" FLOAT,
+    "8:00:00" FLOAT,
+    "8:30:00" FLOAT,
+    "9:00:00" FLOAT,
+    "9:30:00" FLOAT,
+    "10:00:00" FLOAT,
+    "10:30:00" FLOAT,
+    "11:00:00" FLOAT,
+    "11:30:00" FLOAT,
+    "12:00:00" FLOAT,
+    "12:30:00" FLOAT,
+    "13:00:00" FLOAT,
+    "13:30:00" FLOAT,
+    "14:00:00" FLOAT,
+    "14:30:00" FLOAT,
+    "15:00:00" FLOAT,
+    "15:30:00" FLOAT,
+    "16:00:00" FLOAT,
+    "16:30:00" FLOAT,
+    "17:00:00" FLOAT,
+    "17:30:00" FLOAT,
+    "18:00:00" FLOAT,
+    "18:30:00" FLOAT,
+    "19:00:00" FLOAT,
+    "19:30:00" FLOAT,
+    "20:00:00" FLOAT,
+    "20:30:00" FLOAT,
+    "21:00:00" FLOAT,
+    "21:30:00" FLOAT,
+    "22:00:00" FLOAT,
+    "22:30:00" FLOAT,
+    "23:00:00" FLOAT,
+    "23:30:00" FLOAT
+);
+--------------------------------------------------------------
+#Subidas
+CREATE TABLE subidas (
+    paradero VARCHAR(255) PRIMARY KEY,
+    Comuna VARCHAR(255),
+    "5:30:00" INT,
+    "6:00:00" INT,
+    "6:30:00" INT,
+    "7:00:00" INT,
+    "7:30:00" INT,
+    "8:00:00" INT,
+    "8:30:00" INT,
+    "9:00:00" INT,
+    "9:30:00" INT,
+    "10:00:00" INT,
+    "10:30:00" INT,
+    "11:00:00" INT,
+    "11:30:00" INT,
+    "12:00:00" INT,
+    "12:30:00" INT,
+    "13:00:00" INT,
+    "13:30:00" INT,
+    "14:00:00" INT,
+    "14:30:00" INT,
+    "15:00:00" INT,
+    "15:30:00" INT,
+    "16:00:00" INT,
+    "16:30:00" INT,
+    "17:00:00" INT,
+    "17:30:00" INT,
+    "18:00:00" INT,
+    "18:30:00" INT,
+    "19:00:00" INT,
+    "19:30:00" INT,
+    "20:00:00" INT,
+    "20:30:00" INT,
+    "21:00:00" INT,
+    "21:30:00" INT,
+    "22:00:00" INT,
+    "22:30:00" INT,
+    "23:00:00" INT,
+    "23:30:00" INT
 );
 --------------------------------------------------------------
 #Llegadas
@@ -21,37 +118,37 @@ CREATE TABLE espera_bus (
     tiempo_llegada_max INTEGER
 );
 ---------------------------------------------------------------
-#alertas
+#Alertas
 CREATE TABLE alertas (
     id SERIAL PRIMARY KEY,
-    pais VARCHAR(2),                 -- Código de país
-    ciudad VARCHAR(100),             -- Ciudad
-    calle VARCHAR(255),              -- Calle
-    tipo_reporte VARCHAR(50),        -- Tipo de reporte (HAZARD, JAM, etc.)
-    subtipo VARCHAR(100),            -- Subtipo de reporte
-    valoracion INTEGER,              -- Calificación del reporte
-    confiabilidad INTEGER,           -- Confiabilidad del reporte
-    confianza INTEGER,               -- Confianza del reporte
-    reportador VARCHAR(100),         -- Usuario que reporta
-    n_pulgarres_arriba INTEGER,      -- Número de "pulgares arriba"
-    n_comentarios INTEGER,           -- Número de comentarios
-    descripcion TEXT,                -- Descripción del reporte
-    info_adicional TEXT,             -- Información adicional
+    country VARCHAR(2),                 -- Código de país
+    city VARCHAR(100),             -- Ciudad
+    street VARCHAR(255),              -- Calle
+    type VARCHAR(50),        -- Tipo de reporte (HAZARD, JAM, etc.)
+    subtype VARCHAR(100),            -- Subtipo de reporte
+    reportRating INTEGER,              -- Calificación del reporte
+    reliability INTEGER,           -- Confiabilidad del reporte
+    confidence INTEGER,               -- Confianza del reporte
+    reportBy VARCHAR(100),         -- Usuario que reporta
+    nThumbsUp INTEGER,      -- Número de "pulgares arriba"
+    nComments INTEGER,           -- Número de comentarios
+    reportDescription TEXT,                -- Descripción del reporte
+    additionalInfodisp TEXT,             -- Información adicional
     uuid UUID,                       -- Identificador único del reporte
     geom GEOMETRY(Point, 4326)       -- Geometría espacial con el sistema de coordenadas WGS84
 );
 ----------------------------------------------------------------
-#disponibilidad / congestion
+#Disponibilidad / Congestión
 CREATE TABLE disponibilidad (
     id SERIAL PRIMARY KEY,
-    inicio VARCHAR(255) NOT NULL,        -- Nombre del origen del tramo
-    fin VARCHAR(255) NOT NULL,           -- Nombre del destino del tramo
-    etiqueta VARCHAR(255),               -- Etiqueta personalizada para el tramo
-    creado TIMESTAMP NOT NULL,           -- Fecha y hora de creación del registro
-    tiempo INTEGER,                      -- Tiempo de recorrido en segundos
-    congestion INTEGER,                  -- Nivel de congestión (0 a 3)
-    habilitado BOOLEAN DEFAULT TRUE,     -- Si el tramo está habilitado (1) o no (0)
-    longitud INTEGER                     -- Longitud del tramo en metros
+    to_name VARCHAR(255) NOT NULL,        -- Nombre del origen del tramo
+    from_name VARCHAR(255) NOT NULL,           -- Nombre del destino del tramo
+    custom_label VARCHAR(255),               -- Etiqueta personalizada para el tramo
+    created_at TIMESTAMP NOT NULL,           -- Fecha y hora de creación del registro
+    time INTEGER,                      -- Tiempo de recorrido en segundos
+    jam_level INTEGER,                  -- Nivel de congestión (0 a 3)
+    enabled BOOLEAN DEFAULT TRUE,     -- Si el tramo está habilitado (1) o no (0)
+    length INTEGER                     -- Longitud del tramo en metros
 );
 ------------------------------------------------------------------
 #estaciones metro
